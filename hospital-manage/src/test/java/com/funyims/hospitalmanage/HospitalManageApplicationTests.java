d7351079f79e39683ebf94b1fb72e1525687daa1
Funyims <2484225472@qq.com>
2022-07-15 08:59:02 +0800

    第三次提交：添加cmn接口

---
+++ hospital-manage/src/test/java/com/funyims/hospitalmanage/HospitalManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.funyims.hospitalmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
